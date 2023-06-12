a2419b74269bd5bda96aa30bbb96796553c28e56
Jaime Villegas <jaimevillegas296@gmail.com>
2023-06-12 11:01:21 -0500

    Config routes and add Users views

---
+++ app/views/users/show.html.rb
@@ -0,0 +1,5 @@
+<html>
+  <body>
+    <h1>Single User</h1>
+  </body>
+</html>
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # root 'pages#hello'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
-  end
+  get "/users", to: "users#index"
+  get "/users/:id", to: "users#show"
+  get "/users/:user_id/posts", to: "posts#index"
+  get "/users/:user_id/posts/:id", to: "posts#show"
 
   # Defines the root path route ("/")
   root "blog_entries#index"
