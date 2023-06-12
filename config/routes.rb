15eacb84752e0c360ead4eb3c01830968f3d0267
Jaime Villegas <jaimevillegas296@gmail.com>
2023-06-12 10:37:52 -0500

    Config root route & create index in blog_entries

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   # root 'pages#hello'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
+
   # Defines the root path route ("/")
+  root "blog_entries#index"
   # root "articles#index"
 end
