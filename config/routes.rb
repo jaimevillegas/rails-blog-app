551b322a6274f272c1382188e9d0a892267d6f9b
Jaime Villegas <jaimevillegas296@gmail.com>
2023-06-12 11:18:14 -0500

    Work on linter messages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@
 
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'users#index'
+  # resources :users, only: [:index, :show] do
+  #   resources :posts, only: [:index, :show]
+  # end
 
   get "/users", to: "users#index"
   get "/users/:id", to: "users#show"
@@ -9,6 +13,6 @@ Rails.application.routes.draw do
   get "/users/:user_id/posts/:id", to: "posts#show"
 
   # Defines the root path route ("/")
-  root "blog_entries#index"
+  # root "blog_entries#index"
   # root "articles#index"
 end
