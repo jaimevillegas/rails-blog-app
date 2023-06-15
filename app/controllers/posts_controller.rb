efbec55badbad7295b985c2aa535beeee9e1c813
Jaime Villegas <jaimevillegas296@gmail.com>
2023-06-15 09:49:48 -0500

    Add methods to posts_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,4 +13,26 @@ class PostsController < ApplicationController
       @post = nil
     end
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(author_id: current_user.id, **post_params)
+
+    if @post.save
+      flash[:notice] = "Post created successfully"
+      redirect_to user_post_path(params[:user_id])
+    else
+      flash.now[:alert] = "Post could not be created"
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
