bc3a1d3bc977061df3281eaeffd2910dd41e34c8
Jaime Villegas <jaimevillegas296@gmail.com>
2023-06-22 09:38:22 -0500

    Modify add_confirmable

--- db/migrate/20230622134444_add_confirmable_to_devise.rb
+++ db/migrate/20230622134444_add_confirmable_to_devise.rb
@@ -2,9 +2,9 @@ class AddConfirmableToDevise < ActiveRecord::Migration[7.0]
     # Note: You can't use change, as User.update_all will fail in the down migration
   def up
     # add_column :users, :confirmed_at, :datetime
-    add_column :users, :confirmation_sent_at, :datetime
+    # add_column :users, :confirmation_sent_at, :datetime
     # add_column :users, :unconfirmed_email, :string # Only if using reconfirmable
-    add_index :users
+    # add_index :users
     # User.reset_column_information # Need for some types of updates, but not for update_all.
     # To avoid a short time window between running the migration and updating all existing
     # users as confirmed, do the following
