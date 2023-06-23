4fd1ea432d8868d6f14b692341f8336ee52d8580
Jaime Villegas <jaimevillegas296@gmail.com>
2023-06-23 10:25:55 -0500

    add Api comments_controller

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,30 @@
+class Api::CommentsController < ApplicationController::Base
+  protect_from_forgery with: :null_session
+
+  def index
+    @comments = Comment.where(post_id: params[:post_id])
+    render json: @comments
+  end
+
+  def create
+    message = 'Verify Credentials'
+    @user = User.find_by_id(params[:user_id])
+    if @user.present?
+      @post = Post.find_by_id(params[:post_id])
+      if @post.present?
+        @comment = Comment.new(author: @user, post: @post, text: params[:text])
+        message = if @comment.save
+                    'Comment Created'
+                  else
+                    'Comment Not Created'
+                  end
+        else
+          message = 'Post Not Found'
+        end
+      else
+        message = 'User Not Found'
+    end
+
+    render json: { message: message }
+  end
+end
