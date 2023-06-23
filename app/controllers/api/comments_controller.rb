ab46049e21bc6f6f1aeefd96970b7aab336b02ed
Jaime Villegas <jaimevillegas296@gmail.com>
2023-06-23 10:37:55 -0500

    Work on getting data

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,4 +1,4 @@
-class Api::CommentsController < ApplicationController::Base
+class Api::CommentsController < ActionController::Base
   protect_from_forgery with: :null_session
 
   def index
--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,6 +1,6 @@
-class Api::PostsController < ActionController:Base
+class Api::PostsController < ActionController::Base
   def index
-    @posts = Post.includes(:comments).where(author_id_id: :user_id)
+    @posts = Post.includes(:comments).where(author_id: :user_id)
     render json: @posts
   end
 end
